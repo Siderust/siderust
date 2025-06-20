ba9d3c40c18279b33a0b8bca0aa454618c42e814
Ramon <vallespuigramon@gmail.com>
2025-06-20 16:48:48 +0200

    Enhance transformation logic for geocentric reference centers by adding aberration handling

--- src/coordinates/transform/centers/mod.rs
+++ src/coordinates/transform/centers/mod.rs
@@ -13,6 +13,7 @@ use crate::astro::aberration::{
     remove_aberration_from_direction,
 };
 
+// ------------- If we transform TO a Geocentric ReferenceCenter, we need to apply aberration ------------------
 impl<C: ReferenceCenter, F: ReferenceFrame> Transform<cartesian::Direction<Geocentric, F>> for cartesian::Direction<C, F>
 where
     C: NonGeocentric,
@@ -22,36 +23,53 @@ where
 {
     #[inline]
     fn transform(&self, jd: JulianDay) -> cartesian::Direction<Geocentric, F> {
+        // 1. Convert to Geocentric Equatorial coordinates
         let geocentric = cartesian::Direction::<Geocentric, F>::from_vec3(
             self.as_vec3()
         );
+        // 2. Transform to Geocentric Equatorial
         let equatorial: cartesian::Direction<Geocentric, Equatorial> = geocentric.transform(jd);
+        // 3. Apply aberration
         let aberrated = apply_aberration_to_direction(
             cartesian::Direction::from_vec3(equatorial.as_vec3()), jd
         );
-        aberrated.transform(jd)
+        // 4. Recover target Frame
+        Transform::<cartesian::Direction<Geocentric, F>>::transform(&aberrated, jd)
     }
 }
 
-impl<C: ReferenceCenter> Transform<cartesian::Direction<C, Equatorial>> for cartesian::Direction<Geocentric, Equatorial>
+// ------------- If we transform FROM a Geocentric ReferenceCenter, we need to remove aberration ------------------
+impl<C, F> Transform<cartesian::Direction<C, F>> for cartesian::Direction<Geocentric, F>
 where
-    C: NonGeocentric
+    C: ReferenceCenter + NonGeocentric,
+    F: MutableFrame,
+    cartesian::Direction<Geocentric, F>: Transform<cartesian::Direction<Geocentric, Equatorial>>, // Required by Aberration
+    cartesian::Direction<Geocentric, Equatorial>: Transform<cartesian::Direction<Geocentric, F>>,
 {
     #[inline]
-    fn transform(&self, jd: JulianDay) -> cartesian::Direction<C, Equatorial> {
-        cartesian::Direction::<C, Equatorial>::from_vec3(
-            remove_aberration_from_direction(*self, jd).as_vec3()
-        )
+    fn transform(&self, jd: JulianDay) -> cartesian::Direction<C, F> {
+        // 1. Transform to Equatorial (already in Geocentric)
+        let equatorial: cartesian::Direction<Geocentric, Equatorial> = self.transform(jd);
+        // 2. Remove aberration
+        let deaberrated =  remove_aberration_from_direction(
+            equatorial, jd
+        );
+        // 3. Recover target Frame
+        let target_center: cartesian::Direction::<Geocentric, F> = deaberrated.transform(jd);
+        // 4. Transform target Center
+        cartesian::Direction::<C, F>::from_vec3(target_center.as_vec3())
     }
 }
 
-impl<C1: ReferenceCenter, C2: ReferenceCenter> Transform<cartesian::Direction<C1, Equatorial>> for cartesian::Direction<C2, Equatorial>
+// ------------- If None of the centers are geocentric, we can just pass the spherical coordinates through -------------
+impl<C1, C2, F> Transform<cartesian::Direction<C1, F>> for cartesian::Direction<C2, F>
 where
-    C1: NonGeocentric,
-    C2: NonGeocentric
+    C1: ReferenceCenter + NonGeocentric,
+    C2: ReferenceCenter + NonGeocentric,
+    F: MutableFrame,
 {
     #[inline]
-    fn transform(&self, _jd: JulianDay) -> cartesian::Direction<C1, Equatorial> {
+    fn transform(&self, _jd: JulianDay) -> cartesian::Direction<C1, F> {
         cartesian::Direction::from_vec3(self.as_vec3())
     }
 }
@@ -65,11 +83,12 @@ impl Transform<cartesian::Direction<Geocentric, Equatorial>>
     }
 }
 
+// ------------- If we transform TO a Geocentric ReferenceCenter, we need to apply aberration -------------------------
 impl<C: ReferenceCenter, F: ReferenceFrame> Transform<spherical::Direction<Geocentric, F>> for spherical::Direction<C, F>
 where
     C: NonGeocentric,
     F: MutableFrame,
-    cartesian::Direction<Geocentric, F>: Transform<cartesian::Direction<Geocentric, Equatorial>>, // Required by Aberration
+    cartesian::Direction<Geocentric, F>: Transform<cartesian::Direction<Geocentric, Equatorial>>,
     cartesian::Direction<Geocentric, Equatorial>: Transform<cartesian::Direction<Geocentric, F>>,
 {
     #[inline]
@@ -80,23 +99,29 @@ where
     }
 }
 
-impl<C: ReferenceCenter> Transform<spherical::Direction<C, Equatorial>> for spherical::Direction<Geocentric, Equatorial>
+// ------------- If we transform FROM a Geocentric ReferenceCenter, we need to remove aberration -----------------------
+impl<C, F> Transform<spherical::Direction<C, F>> for spherical::Direction<Geocentric, F>
 where
-    C: NonGeocentric,
+    C: ReferenceCenter + NonGeocentric,
+    F: MutableFrame,
+    cartesian::Direction<Geocentric, F>: Transform<cartesian::Direction<Geocentric, Equatorial>>,
+    cartesian::Direction<Geocentric, Equatorial>: Transform<cartesian::Direction<Geocentric, F>>,
 {
     #[inline]
-    fn transform(&self, jd: JulianDay) -> spherical::Direction<C, Equatorial> {
+    fn transform(&self, jd: JulianDay) -> spherical::Direction<C, F> {
         let cart = self.to_cartesian();
-        let catr_trasnformed: cartesian::Direction<C, Equatorial> = cart.transform(jd);
-        catr_trasnformed.to_spherical()
+        let cart_transformed = Transform::<cartesian::Direction<C, F>>::transform(&cart, jd);
+        cart_transformed.to_spherical()
     }
 }
 
-// If None of the centers are geocentric, we can just pass the spherical coordinates through
-impl<C1: ReferenceCenter, C2: ReferenceCenter, F: ReferenceFrame> Transform<spherical::Direction<C1, F>> for spherical::Direction<C2, F>
+
+// ------------- If None of the centers are geocentric, we can just pass the spherical coordinates through -------------
+impl<C1, C2, F> Transform<spherical::Direction<C1, F>> for spherical::Direction<C2, F>
 where
-    C1: NonGeocentric,
-    C2: NonGeocentric
+    C1: ReferenceCenter + NonGeocentric,
+    C2: ReferenceCenter + NonGeocentric,
+    F: MutableFrame,
 {
     #[inline]
     fn transform(&self, _jd: JulianDay) -> spherical::Direction<C1, F> {
@@ -142,7 +167,6 @@ mod tests {
         assert!(!transformed.azimuth.as_f64().is_nan(), "Azimuth should not be NaN");
     }
 
-/*
     #[test]
     fn test_barycentric_to_heliocentric() {
         let dir = Direction::<Barycentric, ICRS>::new(Degrees::new(200.0), Degrees::new(45.0));
@@ -160,5 +184,5 @@ mod tests {
         assert!(!transformed.polar.as_f64().is_nan(), "Polar should not be NaN");
         assert!(!transformed.azimuth.as_f64().is_nan(), "Azimuth should not be NaN");
     }
-    */
+
 }
