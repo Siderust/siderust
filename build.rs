effee6fee1869d94ab379e1672fe92db07556e3e
Ramon <vallespuigramon@gmail.com>
2025-06-22 17:39:22 +0200

    Refactor build script: improve comments for clarity and understanding

--- build.rs
+++ build.rs
@@ -4,15 +4,13 @@ mod vsop87_build;
 use std::{env, path::PathBuf};
 
 fn main() {
-    // OUT_DIR siempre existe y lo da Cargo:
     let out_dir = PathBuf::from(
         env::var("OUT_DIR").expect("OUT_DIR not set by Cargo"),
     );
 
-    // Subcarpeta dentro de OUT_DIR para el dataset
     let data_dir = out_dir.join("vsop87_dataset");
 
-    // run necesita &Path
+    // Download the vosp87 data and auto-generate the Rust source code.
     vsop87_build::run(data_dir.as_path())
         .expect("VSOP87 codegen failed");
 }
