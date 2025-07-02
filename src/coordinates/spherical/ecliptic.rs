a365dd06503d1ebbecc410ee8ba986fdc25d4950
VPRamon <vallespuigramon@gmail.com>
2025-07-02 23:01:13 +0200

    Refactor coordinate types to improve naming consistency and enhance clarity in examples

--- src/coordinates/spherical/ecliptic.rs
+++ src/coordinates/spherical/ecliptic.rs
@@ -22,10 +22,10 @@
 //!
 //! # Example
 //! ```rust
-//! use siderust::coordinates::spherical::EclipticHeliocentricPos;
+//! use siderust::coordinates::spherical::EclipticPos;
 //! use siderust::units::Degrees;
 //!
-//! let coord = EclipticHeliocentricPos::new(
+//! let coord = EclipticPos::new(
 //!     Degrees::new(120.0), Degrees::new(5.0), 1.0
 //! );
 //! println!("lon = {}, lat = {}", coord.lon(), coord.lat());
