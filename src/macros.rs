6a820a0990f6c3612e763975662f293504e01731
VPRamon <vallespuigramon@gmail.com>
2025-07-02 21:11:19 +0200

    Refactor assertion functions to include unit parameter for improved type safety in Cartesian and Spherical position comparisons.

--- src/macros.rs
+++ src/macros.rs
@@ -1,20 +1,21 @@
 // src/macros.rs
 use core::f64;
 use crate::coordinates::{ cartesian, spherical, centers::ReferenceCenter, frames::ReferenceFrame };
+use crate::units::Unit;
 
 #[doc(hidden)]
-pub(crate) fn __assert_cartesian_eq<C, F>(
-    a: &cartesian::Position<C, F>,
-    b: &cartesian::Position<C, F>,
+pub(crate) fn __assert_cartesian_eq<C, F, U>(
+    a: &cartesian::Position<C, F, U>,
+    b: &cartesian::Position<C, F, U>,
     epsilon: f64,
     msg: Option<String>,
 )
-where C: ReferenceCenter, F: ReferenceFrame
+where C: ReferenceCenter, F: ReferenceFrame, U: Unit
 {
     let dx = (a.x() - b.x()).abs();
     let dy = (a.y() - b.y()).abs();
     let dz = (a.z() - b.z()).abs();
-    if dx >= epsilon || dy >= epsilon || dz >= epsilon {
+    if dx >= epsilon.into() || dy >= epsilon.into() || dz >= epsilon.into() {
         if let Some(m) = msg {
             panic!("{}. Cartesian coords differ: {} vs {} (ε = {})", m, a, b, epsilon);
         } else {
@@ -24,19 +25,19 @@ where C: ReferenceCenter, F: ReferenceFrame
 }
 
 #[doc(hidden)]
-pub(crate) fn __assert_spherical_eq<C, F>(
-    a: &spherical::Position<C, F>,
-    b: &spherical::Position<C, F>,
+pub(crate) fn __assert_spherical_eq<C, F,  U>(
+    a: &spherical::Position<C, F, U>,
+    b: &spherical::Position<C, F, U>,
     epsilon: f64,
     msg: Option<String>,
 )
-where C: ReferenceCenter, F: ReferenceFrame
+where C: ReferenceCenter, F: ReferenceFrame, U: Unit
 {
-    let d1 = a.distance.unwrap_or(f64::NAN);
-    let d2 = b.distance.unwrap_or(f64::NAN);
+    let d1 = a.distance.unwrap_or(U::NAN);
+    let d2 = b.distance.unwrap_or(U::NAN);
     let dp = (a.polar.as_f64()   - b.polar.as_f64()).abs();
     let da = (a.azimuth.as_f64() - b.azimuth.as_f64()).abs();
-    if (d1 - d2).abs() >= epsilon || dp >= epsilon || da >= epsilon {
+    if (d1 - d2).abs() >= epsilon.into() || dp >= epsilon || da >= epsilon {
         if let Some(m) = msg {
             panic!("{}. Spherical coords differ: {} vs {} (ε = {})", m, a, b, epsilon);
         } else {
