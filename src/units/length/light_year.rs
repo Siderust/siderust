a2714ef7061ab77303db7318b29b51d3b9e15915
Ramon <vallespuigramon@gmail.com>
2025-07-10 20:23:32 +0200

    cleanup

--- src/units/length/light_year.rs
+++ src/units/length/light_year.rs
@@ -2,40 +2,12 @@
 //!
 //! Provides a strongly-typed representation of a length in Light Years (LY)
 //! and conversions to and from Astronomical Units (AU).
-/*
-use super::Kilometers;
-
-pub const LY: LightYear = LightYear::new(1.0);
-
-/// A strongly-typed representation of a length in Light Years (LY).
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-pub struct LightYear(f64);
-*/
 
 use super::*;
 
 impl LightYears {
     pub const AU_PER_LY: f64 = 63_241.1;
     pub const KM_PER_LY: f64 = 9_460_730_472_580.8;
-
-    /// Creates a new `LightYear` from a value in LY.
-    ///
-    /// # Arguments
-    /// * `value` - The length in light years.
-    ///
-    /// # Example
-    /// ```
-    /// use siderust::units::LightYear;
-    /// let ly = LightYear::new(1.0);
-    /// ```
-    //pub const fn new(value: f64) -> Self {
-    //    Self(value)
-    // }
-
-    // Returns the inner value in LY.
-    //pub const fn value(&self) -> f64 {
-    //    self.0
-    //}
 
     pub const fn to_km(&self) -> Kilometers {
         Kilometers::new(self.0 * Self::KM_PER_LY)
