00153544961852d08f7e70c084e7e07b07d6fa52
VPRamon <vallespuigramon@gmail.com>
2025-07-27 23:55:59 +0200

    wip generalize transformations

--- src/calculus/solar/sun_equations.rs
+++ src/calculus/solar/sun_equations.rs
@@ -2,13 +2,12 @@ use crate::bodies::solar_system::Sun;
 
 use crate::coordinates::{
     cartesian, spherical,
-    centers::Geocentric,
-    frames::Ecliptic
+    centers::*,
+    transform::Transform,
 };
-use crate::units::{Quantity, AstronomicalUnits, LengthUnit, Degrees};
+use crate::units::{Quantity, AstronomicalUnits, LengthUnit};
 use crate::astro::JulianDate;
 
-use crate::astro::nutation::get_nutation;
 impl Sun {
     /// Returns the **apparent geocentric equatorial coordinates** of the Sun
     /// at a given Julian day.
@@ -37,20 +36,14 @@ impl Sun {
     /// visualization.
     pub fn get_apparent_geocentric_equ<U: LengthUnit>(jd: JulianDate) -> spherical::position::Equatorial<U>
     where
-        Quantity<U>: From<AstronomicalUnits> + PartialEq + std::fmt::Debug,
+        Quantity<U>: From<AstronomicalUnits>,
     {
-        let helio = cartesian::position::Ecliptic::<U>::CENTER;
-        let geo_cart: cartesian::Position<Geocentric, Ecliptic, U> = (&helio).into();
-        let mut geo = geo_cart.to_spherical();
+        let helio = cartesian::position::Ecliptic::<U, Heliocentric>::CENTER;
+        let geo_cart: cartesian::position::Equatorial<U, Geocentric> = helio.transform(jd);
+        geo_cart.to_spherical()
 
-        // Apply nutation in ecliptic longitude
-        let nutation = get_nutation(jd);
-        geo.azimuth += nutation.longitude;
-
-        // Apply aberration correction (simplified constant formula)
-        let aberration = (20.4898 / (360.0 * 60.0 * 60.0)) / geo.distance.value();
-        geo.azimuth -= Degrees::new(aberration);
-
-        (&geo).into()
+        // TODO: Apply nutation in ecliptic longitude
+        //let nutation = get_nutation(jd);
+        //geo.azimuth += nutation.longitude;
     }
 }
