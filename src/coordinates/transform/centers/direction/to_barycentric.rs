a86b3566f9a98626b945f2cd650c655a9f783db4
ramon.valles <ramon.valles-puig@cta-consortium.org>
2025-07-27 22:28:50 +0200

    direction uses new transform

--- src/coordinates/transform/centers/direction/to_barycentric.rs
+++ src/coordinates/transform/centers/direction/to_barycentric.rs
@@ -2,7 +2,8 @@ use crate::astro::JulianDate;
 use crate::coordinates::{
     centers::*,
     frames::MutableFrame,
-    cartesian::direction::{Direction, Equatorial}
+    transform::TransformFrame,
+    cartesian::direction::{Direction, Equatorial},
 };
 use crate::coordinates::transform::Transform;
 use crate::coordinates::transform::centers::TransformCenter;
@@ -22,19 +23,17 @@ where
 impl<F: MutableFrame> TransformCenter<Direction<Barycentric, F>>
     for Direction<Geocentric, F>
 where
-    Direction<Geocentric, F>: Transform<Equatorial>, // ToEquatorial
-    Equatorial: Transform<Direction<Geocentric, F>>, // FromEquatorial
+    Direction<Geocentric, F>: TransformFrame<Equatorial>, // ToEquatorial
+    Equatorial: TransformFrame<Direction<Geocentric, F>>, // FromEquatorial
 {
     #[inline]
     fn to_center(&self, jd: JulianDate) -> Direction<Barycentric, F> {
         // 1. Transform to Equatorial (already in Geocentric)
-        let equatorial: Equatorial = self.transform(jd);
+        let equatorial: Equatorial = self.to_frame();
         // 2. Remove aberration
-        let deaberrated =  remove_aberration_from_direction(
-            equatorial, jd
-        );
+        let deaberrated =  remove_aberration_from_direction(equatorial, jd);
         // 3. Recover target Frame
-        let target_center: Direction::<Geocentric, F> = deaberrated.transform(jd);
+        let target_center: Direction::<Geocentric, F> = deaberrated.to_frame();
         // 4. Transform target Center
         Direction::<Barycentric, F>::from_vec3(target_center.as_vec3())
     }
@@ -54,8 +53,8 @@ where
 impl<F: MutableFrame> Transform<Direction<Barycentric, F>>
     for Direction<Geocentric, F>
 where
-    Direction<Geocentric, F>: Transform<Equatorial>, // ToEquatorial
-    Equatorial: Transform<Direction<Geocentric, F>>, // FromEquatorial
+    Direction<Geocentric, F>: TransformFrame<Equatorial>, // ToEquatorial
+    Equatorial: TransformFrame<Direction<Geocentric, F>>, // FromEquatorial
 {
     #[inline]
     fn transform(&self, jd: JulianDate) -> Direction<Barycentric, F> {
