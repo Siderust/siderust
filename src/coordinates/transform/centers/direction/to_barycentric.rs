483e143a2f9308d347931cc22262419da70aa832
ramon.valles <ramon.valles-puig@cta-consortium.org>
2025-07-27 21:33:12 +0200

    wip transform to_center

--- src/coordinates/transform/centers/direction/to_barycentric.rs
+++ src/coordinates/transform/centers/direction/to_barycentric.rs
@@ -5,27 +5,28 @@ use crate::coordinates::{
     cartesian::direction::{Direction, Equatorial}
 };
 use crate::coordinates::transform::Transform;
+use crate::coordinates::transform::centers::TransformCenter;
 use crate::astro::aberration::remove_aberration_from_direction;
 
 // Heliocentric To Barycentric
-impl<F: MutableFrame> Transform<Direction<Barycentric, F>>
+impl<F: MutableFrame> TransformCenter<Direction<Barycentric, F>>
     for Direction<Heliocentric, F>
 where
 {
-    fn transform(&self, _jd: JulianDate) -> Direction<Barycentric, F> {
+    fn to_center(&self, _jd: JulianDate) -> Direction<Barycentric, F> {
         Direction::from_vec3(self.as_vec3())
     }
 }
 
 // Geocentric To Barycentric
-impl<F: MutableFrame> Transform<Direction<Barycentric, F>>
+impl<F: MutableFrame> TransformCenter<Direction<Barycentric, F>>
     for Direction<Geocentric, F>
 where
     Direction<Geocentric, F>: Transform<Equatorial>, // ToEquatorial
     Equatorial: Transform<Direction<Geocentric, F>>, // FromEquatorial
 {
     #[inline]
-    fn transform(&self, jd: JulianDate) -> Direction<Barycentric, F> {
+    fn to_center(&self, jd: JulianDate) -> Direction<Barycentric, F> {
         // 1. Transform to Equatorial (already in Geocentric)
         let equatorial: Equatorial = self.transform(jd);
         // 2. Remove aberration
@@ -36,5 +37,28 @@ where
         let target_center: Direction::<Geocentric, F> = deaberrated.transform(jd);
         // 4. Transform target Center
         Direction::<Barycentric, F>::from_vec3(target_center.as_vec3())
+    }
+}
+
+
+//TODO: REMOVE ME
+impl<F: MutableFrame> Transform<Direction<Barycentric, F>>
+    for Direction<Heliocentric, F>
+where
+{
+    fn transform(&self, _jd: JulianDate) -> Direction<Barycentric, F> {
+        self.to_center(_jd)
+    }
+}
+
+impl<F: MutableFrame> Transform<Direction<Barycentric, F>>
+    for Direction<Geocentric, F>
+where
+    Direction<Geocentric, F>: Transform<Equatorial>, // ToEquatorial
+    Equatorial: Transform<Direction<Geocentric, F>>, // FromEquatorial
+{
+    #[inline]
+    fn transform(&self, jd: JulianDate) -> Direction<Barycentric, F> {
+        self.to_center(jd)
     }
 }
--- src/coordinates/transform/centers/direction/to_geocentric.rs
+++ src/coordinates/transform/centers/direction/to_geocentric.rs
@@ -1,26 +1,24 @@
 use crate::astro::aberration::apply_aberration_to_direction;
 use crate::coordinates::{
     transform::Transform,
+    transform::centers::TransformCenter,
     frames::MutableFrame,
     centers::*,
-    spherical,
     cartesian::direction::{Direction, Equatorial}
 };
 use crate::astro::JulianDate;
 
-// ------------- If we transform TO a Geocentric Direction, we only need to apply aberration ------------------
-impl<C, F> Transform<Direction<Geocentric, F>> for Direction<C, F>
+// Heliocentric To Geocentric
+impl<F: MutableFrame> TransformCenter<Direction<Geocentric, F>>
+    for Direction<Heliocentric, F>
 where
-    C: ReferenceCenter + NonGeocentric,
-    F: MutableFrame,
-    Direction<Geocentric, F>: Transform<Equatorial>, // Required by Aberration
+    Direction<Geocentric, F>: Transform<Equatorial>,
     Equatorial: Transform<Direction<Geocentric, F>>,
 {
-    #[inline]
-    fn transform(&self, jd: JulianDate) -> Direction<Geocentric, F> {
-        // 1. Convert to Geocentric Equatorial coordinates
+    fn to_center(&self, jd: JulianDate) -> Direction<Geocentric, F> {
+        // 1. Convert to Geocentric coordinates
         let geocentric = Direction::<Geocentric, F>::from_vec3(self.as_vec3());
-        // 2. Transform to Geocentric Equatorial
+        // 2. Transform to Equatorial
         let equatorial: Equatorial<Geocentric> = geocentric.transform(jd);
         // 3. Apply aberration
         let aberrated = apply_aberration_to_direction(
@@ -32,17 +30,24 @@ where
     }
 }
 
-impl<C, F> Transform<spherical::Direction<Geocentric, F>> for spherical::Direction<C, F>
+// Barycentric To Geocentric
+impl<F: MutableFrame> TransformCenter<Direction<Geocentric, F>>
+    for Direction<Barycentric, F>
 where
-    C: ReferenceCenter + NonGeocentric,
-    F: MutableFrame,
     Direction<Geocentric, F>: Transform<Equatorial>,
     Equatorial: Transform<Direction<Geocentric, F>>,
 {
-    #[inline]
-    fn transform(&self, jd: JulianDate) -> spherical::Direction<Geocentric, F> {
-        let cart = self.to_cartesian();
-        let cart_transformed = Transform::<Direction<Geocentric, F>>::transform(&cart, jd);
-        cart_transformed.to_spherical()
+    fn to_center(&self, jd: JulianDate) -> Direction<Geocentric, F> {
+        // 1. Convert to Geocentric coordinates
+        let geocentric = Direction::<Geocentric, F>::from_vec3(self.as_vec3());
+        // 2. Transform to Equatorial
+        let equatorial: Equatorial<Geocentric> = geocentric.transform(jd);
+        // 3. Apply aberration
+        let aberrated = apply_aberration_to_direction(
+            Equatorial::<Geocentric>::from_vec3(equatorial.as_vec3()),
+            jd,
+        );
+        // 4. Recover target Frame
+        Transform::<Direction<Geocentric, F>>::transform(&aberrated, jd)
     }
 }
