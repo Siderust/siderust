d3e8eba895d124f1ec0860213a146960d8692f4d
VPRamon <vallespuigramon@gmail.com>
2025-07-27 23:28:16 +0200

    impl generic direction center transform

--- src/coordinates/transform/centers/direction/mod.rs
+++ src/coordinates/transform/centers/direction/mod.rs
@@ -1,3 +1,24 @@
 pub mod to_barycentric;
 pub mod to_heliocentric;
 pub mod to_geocentric;
+
+use crate::astro::JulianDate;
+use crate::coordinates::{
+    cartesian,
+    frames::ReferenceFrame,
+    centers::ReferenceCenter,
+    transform::Transform,
+    transform::TransformCenter,
+};
+
+impl<C1, C2, F> Transform<cartesian::Direction<C1, F>> for cartesian::Direction<C2, F>
+where
+    C1: ReferenceCenter,
+    C2: ReferenceCenter,
+    F: ReferenceFrame,
+    cartesian::Direction<C2, F>: TransformCenter<cartesian::Direction<C1, F>>
+{
+    fn transform(&self, jd: JulianDate) -> cartesian::Direction<C1, F> {
+        self.to_center(jd)
+    }
+}
