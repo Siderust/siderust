b513bbf2d0e7c60f79c5b0804fc22aaebd58459d
VPRamon <vallespuigramon@gmail.com>
2025-07-27 21:33:12 +0200

    wip transform to_center

--- src/coordinates/transform/centers/direction/to_heliocentric.rs
+++ src/coordinates/transform/centers/direction/to_heliocentric.rs
@@ -5,27 +5,28 @@ use crate::coordinates::{
     cartesian::direction::{Direction, Equatorial}
 };
 use crate::coordinates::transform::Transform;
+use crate::coordinates::transform::centers::TransformCenter;
 use crate::astro::aberration::remove_aberration_from_direction;
 
 // Barycentric To Heliocentric
-impl<F: MutableFrame> Transform<Direction<Heliocentric, F>>
+impl<F: MutableFrame> TransformCenter<Direction<Heliocentric, F>>
     for Direction<Barycentric, F>
 where
 {
-    fn transform(&self, _jd: JulianDate) -> Direction<Heliocentric, F> {
+    fn to_center(&self, _jd: JulianDate) -> Direction<Heliocentric, F> {
         Direction::from_vec3(self.as_vec3())
     }
 }
 
 // Geocentric To Heliocentric
-impl<F: MutableFrame> Transform<Direction<Heliocentric, F>>
+impl<F: MutableFrame> TransformCenter<Direction<Heliocentric, F>>
     for Direction<Geocentric, F>
 where
     Direction<Geocentric, F>: Transform<Equatorial>, // ToEquatorial
     Equatorial: Transform<Direction<Geocentric, F>>, // FromEquatorial
 {
     #[inline]
-    fn transform(&self, jd: JulianDate) -> Direction<Heliocentric, F> {
+    fn to_center(&self, jd: JulianDate) -> Direction<Heliocentric, F> {
         // 1. Transform to Equatorial (already in Geocentric)
         let equatorial: Equatorial = self.transform(jd);
         // 2. Remove aberration
@@ -36,5 +37,28 @@ where
         let target_center: Direction::<Geocentric, F> = deaberrated.transform(jd);
         // 4. Transform target Center
         Direction::<Heliocentric, F>::from_vec3(target_center.as_vec3())
+    }
+}
+
+
+// Barycentric To Heliocentric
+impl<F: MutableFrame> Transform<Direction<Heliocentric, F>>
+    for Direction<Barycentric, F>
+where
+{
+    fn transform(&self, _jd: JulianDate) -> Direction<Heliocentric, F> {
+        self.to_center(_jd)
+    }
+}
+
+impl<F: MutableFrame> Transform<Direction<Heliocentric, F>>
+    for Direction<Geocentric, F>
+where
+    Direction<Geocentric, F>: Transform<Equatorial>, // ToEquatorial
+    Equatorial: Transform<Direction<Geocentric, F>>, // FromEquatorial
+{
+    #[inline]
+    fn transform(&self, jd: JulianDate) -> Direction<Heliocentric, F> {
+        self.to_center(jd)
     }
 }
