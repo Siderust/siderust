85fb25d796a1ef9776fed632de7c1a03b8429e89
Vallés Puig, Ramon <74069811+VPRamon@users.noreply.github.com>
2025-08-10 00:36:19 +0200

    feat(units): add generic per units

--- src/units/frequency.rs
+++ src/units/frequency.rs
@@ -1,71 +1,26 @@
 //! # Frequency Units Module
 //!
-//! This module provides types and utilities for handling frequency-related calculations
-//! in astronomical and scientific contexts. It includes representations for angular
-//! frequency such as radians per day and conversions from angular and time units.
-//!
-//! ## Features
-//! - **RadiansPerDay**: Represents angular frequency in radians per day.
-//! - Arithmetic operations for frequency types.
-//! - Conversion from `Radians` and `Days`.
-//!
-//! ## Example Usage
-//! ```rust
-//! use siderust::units::{Radians, Days, RadiansPerDay};
-//!
-//! let angle = Radians::new(std::f64::consts::PI);
-//! let time = Days::new(2.0);
-//! let frequency = angle / time; // RadiansPerDay
-//! assert_eq!(frequency.value(), std::f64::consts::PI / 2.0);
-//! ```
+//! Frequency units are represented as a ratio between an angular measure and a
+//! time unit using the generic [`Per<N, D>`] type. This enables automatic
+//! multiplication and division without bespoke implementations for every pair.
 
 use super::*;
 
-pub enum Frequency {}
-impl Dimension for Frequency {}
+/// Dimension alias for angular frequency (`Angular / Time`).
+pub type Frequency = DivDim<Angular, Time>;
+
+/// Marker trait for any unit with frequency dimension.
 pub trait FrequencyUnit: Unit<Dim = Frequency> {}
 impl<T: Unit<Dim = Frequency>> FrequencyUnit for T {}
 
-macro_rules! frequency_unit_auto {
-    ($Num:ident, $Den:ident) => {
-        paste::paste! {
-            define_unit!(
-                concat!($Num::SYMBOL, "/", $Den::SYMBOL),
-                [<$Num Per $Den>],
-                Frequency,
-                $Num::RATIO / $Den::RATIO
-            );
-            pub type [<$Num sPer $Den>] = Quantity<[<$Num Per $Den>]>;
-            
-            impl std::ops::Div<Quantity<$Den>> for Quantity<$Num> {
-                type Output = Quantity<[<$Num Per $Den>]>;
+pub type RadianPerDay = Per<Radian, Day>;
+pub type RadiansPerDay = Quantity<RadianPerDay>;
 
-                fn div(self, rhs: Quantity<$Den>) -> Self::Output {
-                    Self::Output::new(self.0 / rhs.0)
-                }
-            }
+pub type DegreePerDay = Per<Degree, Day>;
+pub type DegreesPerDay = Quantity<DegreePerDay>;
 
-            impl std::ops::Div<Quantity<[<$Num Per $Den>]>> for Quantity<$Num> {
-                type Output = Quantity<$Den>;
+pub type DegreePerYear = Per<Degree, Year>;
+pub type DegreesPerYear = Quantity<DegreePerYear>;
 
-                fn div(self, rhs: Quantity<[<$Num Per $Den>]>) -> Self::Output {
-                    Self::Output::new(self.value() / rhs.value())
-                }
-            }
-
-            impl std::ops::Mul<Quantity<$Den>> for  Quantity<[<$Num Per $Den>]> {
-                type Output = Quantity<$Num>;
-
-                fn mul(self, rhs: Quantity<$Den>) -> Self::Output {
-                    Self::Output::new(self.0 * rhs.value())
-                }
-            }
-
-        }
-    };
-}
-
-frequency_unit_auto!(Radian, Day);      // "rad/day"
-frequency_unit_auto!(Degree, Day);      // "deg/day"
-frequency_unit_auto!(Degree, Year);     // "deg/year"
-frequency_unit_auto!(MilliArcsecond, Day); // "mas/day"
+pub type MilliArcsecondPerDay = Per<MilliArcsecond, Day>;
+pub type MilliArcsecondsPerDay = Quantity<MilliArcsecondPerDay>;
