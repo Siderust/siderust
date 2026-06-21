f34112436451666ca7f9bd351adb1322749acd51
Vallés Puig, Ramon <74069811+VPRamon@users.noreply.github.com>
2026-06-21 16:43:21 +0200

    Expose HEALPix test module

--- src/healpix/mod.rs
+++ src/healpix/mod.rs
@@ -7,14 +7,15 @@ mod ordering;
 mod ring;
 mod validation;
 
+#[cfg(test)]
+mod tests;
+
 pub use error::{HealpixError, Result};
 pub use grid::HealpixGrid;
 pub use index::HealpixIndex;
 pub use map::HealpixMap;
 pub use nside::{Nside, MAX_NSIDE};
 pub use ordering::HealpixOrdering;
-
 pub(crate) use validation::validate_healpix_map_complete;
-
 #[cfg(test)]
 pub(crate) use ring::direction_from_lon_lat_rad;
